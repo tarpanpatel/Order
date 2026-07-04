400026a65d8d5b891b1bca43cd96619e6eb1483c
tarpanpatel <tarpan.a.patel@gmail.com>
2026-07-04 18:27:57 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -131,7 +131,8 @@ $all_booked_guests    = $pdo->query("SELECT id, guest_name FROM guests WHERE sta
     <strong style="font-size: 13px; text-transform: uppercase; letter-spacing: 0.5px; color: var(--text-main);">POS Dashboard</strong>
 </div>
 
-<div class="app-container" <?php echo ($is_staff_role && $current_page === 'kitchen.php') ? 'style="grid-template-columns: 1fr;"' : 'style=""'; ?>>    
+<div class="app-container" <?= ($is_staff_role && $current_page === 'kitchen.php') ? 'style="grid-template-columns: 1fr;"' : ''; ?>>
+    
     <?php if (!($is_staff_role && $current_page === 'kitchen.php')): ?>
     <div class="sidebar" id="appLeftNavigationMenu">
         
