dba5a63df999a9a828785db3719572f8c6488921
tarpanpatel <tarpan.a.patel@gmail.com>
2026-07-06 20:43:05 +0530

    Update menu_admin.php

--- menu_admin.php
+++ menu_admin.php
@@ -1,10 +1,11 @@
 <?php
-// /home/apartment/artistsfarmjaipur.com/Order/menu_admin.php
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
 require_once "config/db.php";
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+// /home/apartment/artistsfarmjaipur.com/Order/menu_admin.php
+if (session_status() === PHP_SESSION_NONE) { session_start(); }
+
 if (!isset($_SESSION["role"]) || ($_SESSION["role"] !== "Admin" && $_SESSION["role"] !== "Super Admin")) {
     die("Access Denied: Administrative credentials required.");
 }
