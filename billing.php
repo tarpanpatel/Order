7473f99e81320efcbd674f1a01508fc25da8aef0
tarpanpatel <tarpan.a.patel@gmail.com>
2026-07-07 13:51:42 +0530

    Update billing.php

--- billing.php
+++ billing.php
@@ -1,5 +1,11 @@
 <?php
 // /home/apartment/artistsfarmjaipur.com/Order/billing.php
+// Report all PHP errors
+error_reporting(E_ALL);
+
+// Display errors directly on the screen
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
