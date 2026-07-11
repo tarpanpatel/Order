24fab619ec8c81cddf67f7526594acb5d18021cd
tarpanpatel <tarpan.a.patel@gmail.com>
2026-07-11 19:18:49 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php
 // /home/apartment/artistsfarmjaipur.com/Order/includes/header.php
 error_reporting(E_ALL);
-ini_set('display_errors', '0');
+ini_set('display_errors', '1');
 
 require_once __DIR__ . "/../config/db.php";
 
