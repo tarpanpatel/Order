5e125286ca0c1f77de22c8b9c0c38275d2b05a91
tarpanpatel <tarpan.a.patel@gmail.com>
2026-07-11 19:19:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 // /home/apartment/artistsfarmjaipur.com/Order/index.php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 require_once "config/db.php";
 
 // --- AJAX/POST HANDLER FOR INCOMING DEFICIENT MATERIALS ---
